b50b9100c3dc53242f6abc8ab3122b317215d5d2
Roshith R <r.roshith@gmail.com>
2021-03-22 08:08:52 +0530

    added form submit and handle change

--- src/components/FormikForms.js
+++ src/components/FormikForms.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 import { TextField, Button } from '@material-ui/core'
+import { useFormik } from 'formik'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,22 +10,51 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }))
-
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  company: '',
+}
 export const FormikForms = () => {
   const classes = useStyles()
-
+  const formik = useFormik({
+    initialValues, onSubmit: val => console.log(val)
+  })
   return (
-    <form className={classes.root} noValidate autoComplete='off'>
+    <form
+      className={classes.root}
+      noValidate
+      autoComplete='off'
+      onSubmit={formik.handleSubmit}
+    >
       <div>
-        <TextField label='First Name' defaultValue='' />
+        <TextField
+          label='First Name'
+          defaultValue=''
+          name='firstName'
+          value={formik.firstName}
+          onChange={formik.handleChange}
+        />
       </div>
       <div>
-        <TextField label='Last Name' defaultValue='' />
+        <TextField
+          label='Last Name'
+          defaultValue=''
+          name='lastName'
+          value={formik.lastName}
+          onChange={formik.handleChange}
+        />
       </div>
       <div>
-        <TextField label='Company' defaultValue='' />
+        <TextField
+          label='Company'
+          defaultValue=''
+          name='company'
+          value={formik.company}
+          onChange={formik.handleChange}
+        />
       </div>
-      <Button variant='contained' color='primary' type="submit">
+      <Button variant='contained' color='primary' type='submit'>
         Submit
       </Button>
     </form>
