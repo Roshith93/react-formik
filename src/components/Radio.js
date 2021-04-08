949037278880fe6b93ba96ee83c7f00326f3db78
Roshith R <r.roshith@gmail.com>
2021-04-08 08:26:24 +0530

    added checked attribute to check value

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -5,14 +5,25 @@ import TextError from './TextError'
 export const Radio = ({ name, label, options, ...rest }) => {
   return (
     <div className='form-control'>
-      {options.map((option) => {
-        return (
-          <React.Fragment key={option.label}>
-            <Field name={name} id={option.label} value={option.value} {...rest} as='input' />
-            <label htmlFor={option.label}>{option.label}</label>
-          </React.Fragment>
-        )
-      })}
+      <label>{label}</label>
+      <Field name={name}>
+        {({ field }) => {
+          return options.map((option) => {
+            return (
+              <React.Fragment key={option.value}>
+                <label htmlFor={option.label}>{option.label}</label>
+                <input
+                type='radio'
+                  id={option.value}
+                  {...field}
+                  value={option.value}
+                  checked={field.value === option.value}
+                />
+              </React.Fragment>
+            )
+          })
+        }}
+      </Field>
 
       <ErrorMessage name={name} component={TextError} />
     </div>
